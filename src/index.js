23c4ae71b852e2ffed5f1ac7aad3e0be48d65cc3
yadneshgondhali28 <yadneshgondhali04@gmail.com>
2024-02-23 23:39:31 +0530

    add more styles

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const templatePath = path.join(__dirname, "../templates");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.engine('hbs', hbs.__express);
 app.set("view engine", 'hbs');
 app.set('views', templatePath);
 
